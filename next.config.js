dec3cfe2073480f7a318d82c063ca5f23c8ea21e
pione3r <dev2diver@gmail.com>
2023-06-11 00:02:31 +0900

    chore: swcMinify / styled SSR

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
