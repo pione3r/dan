54dd65475b1630c5c7059eadaa0f1c85c898fa00
pione3r <dev2diver@gmail.com>
2023-06-19 12:52:19 +0900

    chore: feed 페이지 init

---
+++ src/app/feed/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { Feed } from "@/components/@피드/organisms/Feed";
+
+export default function FeedPage() {
+  return <Feed />;
+}
---
+++ src/components/@피드/organisms/Feed/index.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export function Feed() {
+  return <div>hi</div>;
+}
